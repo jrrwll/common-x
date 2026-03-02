2650cf21fe6fc993700ffbeba02f3c18362f7a4a
tuke <jrriwll@gmail.com>
2026-03-03 00:10:55 +0800

    commit

--- common-web/src/main/java/org/dreamcat/common/web/mybatis/MybatisUtil.java
+++ common-web/src/main/java/org/dreamcat/common/web/mybatis/MybatisUtil.java
@@ -1,20 +1,37 @@
 package org.dreamcat.common.web.mybatis;
 
 import java.io.StringReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.apache.ibatis.builder.xml.XMLConfigBuilder;
+import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
+import org.apache.ibatis.mapping.ParameterMode;
 import org.apache.ibatis.parsing.GenericTokenParser;
 import org.apache.ibatis.parsing.TokenHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.scripting.defaults.DefaultParameterHandler;
 import org.apache.ibatis.scripting.xmltags.DynamicContext;
 import org.apache.ibatis.scripting.xmltags.DynamicSqlSource;
 import org.apache.ibatis.scripting.xmltags.OgnlCache;
 import org.apache.ibatis.scripting.xmltags.SqlNode;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.type.SimpleTypeRegistry;
+import org.dreamcat.common.Pair;
+import org.dreamcat.common.util.DateUtil;
+import org.dreamcat.common.util.ObjectUtil;
 import org.dreamcat.common.util.ReflectUtil;
 import org.dreamcat.common.util.StringUtil;
 
@@ -26,19 +43,114 @@ public class MybatisUtil {
 
     private static final String template = "<!DOCTYPE configuration PUBLIC \"-//mybatis.org//DTD Config 3.0//EN\" \"http://mybatis.org/dtd/mybatis-3-config.dtd\"><configuration><mappers><mapper resource=\"%s\"/></mappers></configuration>";
 
-    public static String getSql(String mapperPath, String mapperCanonicalName, Object bean) {
+    @SneakyThrows
+    public static Pair<PreparedStatement, String> getPreparedStatementAndFinalSql(
+            Connection connection, Configuration configuration, String mappedStatementId, Object bean) {
+        MappedStatement mappedStatement = configuration.getMappedStatement(mappedStatementId);
+        if (mappedStatement == null) {
+            return null;
+        }
+        BoundSql boundSql = mappedStatement.getBoundSql(bean);
+        String sql = boundSql.getSql();
+
+        PreparedStatement statement = connection.prepareStatement(sql);
+        ParameterHandler parameterHandler = new DefaultParameterHandler(mappedStatement, bean, boundSql);
+        parameterHandler.setParameters(statement);
+
+        List<Object> parameterValues = getParameterValues(configuration, boundSql);
+        String finalSql = getFinalSql(sql, parameterValues);
+        return Pair.of(statement, finalSql);
+    }
+
+    private static List<Object> getParameterValues(Configuration configuration, BoundSql boundSql) {
+        List<Object> paramValues = new ArrayList<>();
+
+        Object parameterObject = boundSql.getParameterObject();
+        List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+        if (ObjectUtil.isEmpty(parameterMappings)) {
+            return paramValues;
+        }
+
+        MetaObject metaObject = null;
+        if (parameterObject != null && !(parameterObject instanceof Map)) {
+            metaObject = configuration.newMetaObject(parameterObject);
+        }
+
+        for (ParameterMapping parameterMapping : parameterMappings) {
+            if (parameterMapping.getMode() == ParameterMode.OUT) {
+                continue;
+            }
+
+            String propertyName = parameterMapping.getProperty();
+
+            Object value;
+            // 1. extra param, such as: <bind>
+            if (boundSql.hasAdditionalParameter(propertyName)) {
+                value = boundSql.getAdditionalParameter(propertyName);
+            }
+            // 2. map
+            else if (parameterObject instanceof Map) {
+                value = ((Map<?, ?>) parameterObject).get(propertyName);
+            }
+            // 3. bean
+            else if (metaObject != null) {
+                value = metaObject.getValue(propertyName);
+            }
+            // 4. single param
+            else {
+                value = parameterObject;
+            }
+
+            if (value != null && ReflectUtil.isCollectionOrArray(value.getClass())) {
+                paramValues.addAll(ReflectUtil.castAsCollection(value));
+            } else {
+                paramValues.add(value);
+            }
+        }
+        return paramValues;
+    }
+
+    private static String getFinalSql(String sqlTemplate, List<Object> parameterValues) {
+        String sql = sqlTemplate;
+        for (Object parameterValue : parameterValues) {
+            String value;
+            if (parameterValue == null) {
+                value = "null";
+            } else if (parameterValue instanceof Number) {
+                value = parameterValue.toString();
+            } else {
+                String literal;
+                if (parameterValue instanceof Date) {
+                    literal = DateUtil.format((Date)parameterValue);
+                } else if (parameterValue instanceof LocalDateTime) {
+                    literal = DateUtil.format((LocalDateTime)parameterValue);
+                } else if (parameterValue instanceof LocalDate) {
+                    literal = DateUtil.formatDate((LocalDate)parameterValue);
+                } else {
+                    literal = StringUtil.escape(parameterValue.toString(), "'");
+                }
+                value = "'" + literal + "'";
+            }
+            sql = sql.replaceFirst("\\?", Matcher.quoteReplacement(value));
+        }
+        return sql;
+    }
+
+    // ==== ==== ==== ====    ==== ==== ==== ====    ==== ==== ==== ====
+
+    public static String getSql(String mapperPath, String mappedStatementId, Object bean) {
         XMLConfigBuilder parser = new XMLConfigBuilder(
                 new StringReader(String.format(template, mapperPath)), null, null);
         Configuration configuration = parser.parse();
 
-        MappedStatement mappedStatement = configuration.getMappedStatement(mapperCanonicalName);
+        MappedStatement mappedStatement = configuration.getMappedStatement(mappedStatementId);
         BoundSql boundSql = mappedStatement.getBoundSql(bean);
         return boundSql.getSql();
     }
 
     @SneakyThrows
-    public static String getFinalSql(Configuration configuration, String mapperCanonicalName, Object bean) {
-        MappedStatement mappedStatement = configuration.getMappedStatement(mapperCanonicalName);
+    public static String getFinalSql(Configuration configuration, String mappedStatementId, Object bean) {
+        MappedStatement mappedStatement = configuration.getMappedStatement(mappedStatementId);
         if (mappedStatement == null) {
             return null;
         }
